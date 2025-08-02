37afd365e246c3ed3caa037814fc7d4acd39837d
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-08-02 14:35:58 +0900

    fix:lampが本番で見られなかったので修正

--- apps/lamp-ui/vite.config.js
+++ apps/lamp-ui/vite.config.js
@@ -1,16 +1,25 @@
-// vite.config.js
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base: env.VITE_BASE_PATH || '/build/',
     plugins: [react()],
     publicDir: 'public',
     build: {
-      outDir: 'dist',
+      outDir: 'public/build',
+      manifest: true,
+      rollupOptions: {
+        input: 'src/main.jsx',
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
   }
 })
