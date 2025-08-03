8f1fe713796cfceac877e0bf9abe2e34b170339a
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-08-03 16:08:06 +0900

    chore:first commit

---
+++ proposal/src/components/StoryBlock/HeroSection.jsx
@@ -0,0 +1,13 @@
+// src/components/HeroSection.jsx
+const HeroSection = () => {
+  return (
+    <section className="relative h-screen text-yellow-200 font-rounded">
+      {/* 背景画像 */}
+
+      {/* オーバーレイ */}
+
+      {/* 中央寄せテキスト（h1 + p） */}
+
+    </section>
+  );
+};
