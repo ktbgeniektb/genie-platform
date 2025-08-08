2628b51945609d35893fc27b2b4a4e8816f36ae2
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-08-08 14:47:37 +0900

    add:ちょっと追加、一時コミット

--- proposal/src/components/Hero.jsx
+++ proposal/src/components/Hero.jsx
@@ -1,20 +1,40 @@
 export default function Hero() {
   return (
-    <section
-      className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-center px-4"
-      style={{ backgroundImage: "url('/img/genie_lp_bg.jpg')" }}
-    >
-      <div className="space-y-6 text-yellow-300">
-        <h2 className="text-2xl md:text-4xl font-semibold font-rounded">
-          「好き」で溢れる日本をつくる
-        </h2>
-        <h1 className="text-3xl md:text-5xl font-bold font-rounded">
-          キャリア教育プラットフォーム<br /><br />
-         <span className="text-8xl font-aladdin">
-          Genie
-          </span>
-        </h1>
-      </div>
-    </section>
+    <div>
+      <section
+        className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-center px-4"
+        style={{ backgroundImage: "url('/img/genie_lp_bg.jpg')" }}
+      >
+      </section>
+      <section className="justify-center items-center text-center">
+        <div className="space-y-6 text-yellow-300">
+          <h2 className="text-2xl md:text-3xl font-bold mb-4">プロダクト名</h2>
+          <h2 className="text-2xl md:text-4xl font-semibold font-rounded drop-shadow-md">
+            「好き」で溢れる日本をつくる
+          </h2>
+          <h1 className="text-3xl md:text-5xl font-bold font-rounded drop-shadow-md">
+            キャリア教育プラットフォーム<br /><br />
+          <span className="text-8xl font-aladdin drop-shadow-md">
+            Genie
+            </span>
+          </h1>
+        </div>
+      </section>
+      <section className=" bg-[#1a1a40] py-16 px-6 text-center text-gray-800">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4">開発者紹介</h2>
+        <div className="max-w-2xl mx-auto space-y-4 text-lg leading-relaxed">
+          <p>はじめまして、水野 敬太です。</p>
+          <p>
+            新卒で株式会社あつまるに入社し、社内のシステム開発に従事するエンジニアです。
+            Web未経験からバックエンドやインフラ構築を経験し、エンジニアグループの地盤を整えてきました。
+          </p>
+          <p>
+            15年間続けたダンスやスポーツの経験から、「人の心が動く瞬間」に強く惹かれ、
+            その価値をテクノロジーで再現したいという想いでこのプロジェクトを立ち上げました。
+          </p>
+        </div>
+      </section>
+    </div>
+
   );
 }
