5a8981c17a0cf6e4ca528646b2bd74ec258f05e5
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-08-17 15:35:03 +0900

    chore:不要なものを削除して必要な設定を追加

--- apps/atlas-ui/src/App.jsx
+++ apps/atlas-ui/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import StudentList from "./pages/StudentList";
 import LoginPage from './pages/LoginPage';
+import axios from "axios";
 
 function App() {
   const basename = import.meta.env.VITE_BASE_PATH || "/";
--- apps/atlas/routes/api.php
+++ apps/atlas/routes/api.php
@@ -3,7 +3,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
     // 認証が必要なAPI
     Route::get('/user', function (Request $request) {
         return $request->user();
@@ -13,4 +13,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/students', [StudentController::class, 'store']);
     Route::put('/students/{id}', [StudentController::class, 'update']);
     Route::delete('/students/{id}', [StudentController::class, 'destroy']);
-});
+// });
