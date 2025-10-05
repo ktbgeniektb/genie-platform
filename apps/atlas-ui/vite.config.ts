169784efef3caa535b8ceadcd8a66ff7bcba160e
水野敬太 <k_mizuno@atsu-maru.co.jp>
2025-10-05 19:51:31 +0900

    fix:設定追加

--- apps/atlas-ui/vite.config.ts
+++ apps/atlas-ui/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/", // ← これを必ず追加
   server: {
-    historyApiFallback: true, // ←これを追加
+    historyApiFallback: true,
+    host: true,
+    port: 5173,
   },
   resolve: {
     alias: {
